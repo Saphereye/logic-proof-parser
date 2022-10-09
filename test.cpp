ebedf61694bae6b5033260b6eecd12556f17bf8a
Saphereye <adarshdas950@gmail.com>
2022-10-09 18:17:46 +0530

    Fixed issue #1

--- test.cpp
+++ test.cpp
@@ -5,11 +5,9 @@
 using namespace std;
 
 int main() {
-    string infix_1 = "(a*b)>(a+c)";
-    string infix_2 = "a*b>a+c";
-
-    assert(Logic::infixToPrefix(infix_1) == ">*ab+ac");
-    assert(Logic::infixToPrefix(infix_2) == ">*ab+ac");
+    assert(Logic::infixToPrefix("(a*b)>(a+c)") == ">*ab+ac");
+    assert(Logic::infixToPrefix("a*b>a+c") == ">*ab+ac");
+    assert(Logic::infixToPrefix("(a*b>a)+c") == "+>*abac");
     return 0;
 
 }
