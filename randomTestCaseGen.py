7dbaa97060e7880a89a58508e8364c23fd30d34a
MaheswarBora <110374795+MaheswarBora@users.noreply.github.com>
2022-10-22 11:48:58 +0530

    Update randomTestCaseGen.py

--- randomTestCaseGen.py
+++ randomTestCaseGen.py
@@ -4,6 +4,7 @@ from secrets import choice
 import threading
 
 infix = ""
-for i in range(1_000):
-    infix += ((i+1)%2)*random.choices(list("abcd"))[0] + (i%2)*random.choice(list("~+*>"))[0]
-print(infix, end='')
+for i in range(1_000_000_00):
+    infix += ((i+1)%2)*random.choices(list("abcdefghijklmnopqrstuvwxyz"))[0] + (i%2)*random.choice(list("~+*>"))[0]
+with open("InfixText.txt","w") as file:
+    file.write(infix)
