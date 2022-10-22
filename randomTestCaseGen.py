b1c7eb454004e287b01d4e4dfbf6525a8df1d1b5
Saphereye <adarshdas950@gmail.com>
2022-10-22 11:21:51 +0530

    Improved profiling tools

--- randomTestCaseGen.py
+++ randomTestCaseGen.py
@@ -4,6 +4,6 @@ from secrets import choice
 import threading
 
 infix = ""
-for i in range(1_000_000):
+for i in range(1_000):
     infix += ((i+1)%2)*random.choices(list("abcd"))[0] + (i%2)*random.choice(list("~+*>"))[0]
 print(infix, end='')
